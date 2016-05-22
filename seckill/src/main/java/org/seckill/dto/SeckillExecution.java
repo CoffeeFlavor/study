5d1e1ad221fdf5605fee57c32657bd8342b76b2c
Coffee Flavor <1239142342@qq.com>
2016-05-22 21:34:20 +0800

    add code

---
+++ seckill/src/main/java/org/seckill/dto/SeckillExecution.java
@@ -0,0 +1,31 @@
+package org.seckill.dto;
+
+import org.seckill.entity.SuccessKilled;
+
+/**
+ * User: jennie
+ * Date: 2016/5/22
+ * Time: 11:30
+ */
+public class SeckillExecution {
+
+    private long seckillId;
+
+    //秒杀执行结果状态
+    private int state;
+
+    //状态表示
+    private String stateInfo;
+
+    //秒杀成功对象
+    private SuccessKilled successKilled;
+
+
+
+    public SeckillExecution(long seckillId, int state, String stateInfo, SuccessKilled successKilled) {
+        this.seckillId = seckillId;
+        this.state = state;
+        this.stateInfo = stateInfo;
+        this.successKilled = successKilled;
+    }
+}
---
+++ seckill/src/main/java/org/seckill/service/SekillService.java
@@ -0,0 +1,44 @@
+package org.seckill.service;
+
+import org.seckill.dto.Exposer;
+import org.seckill.entity.Seckill;
+
+import java.util.List;
+
+/**
+ * User: jennie
+ * Date: 2016/5/22
+ * Time: 10:58
+ * 业务接口：站在“使用者”角度设计接口
+ * 三个方面:方法定义粒度，参数，返回类型（return 类型/异常）
+ */
+public interface SekillService {
+
+    /**
+     * 查询所有秒杀记录
+     * @return
+     */
+    List<Seckill> getSeckillList();
+
+    /**
+     * 查询单个秒杀记录
+     * @param seckillId
+     * @return
+     */
+    Seckill getById(long seckillId);
+
+    /**
+     * 秒杀开启时输出秒杀接口地址，
+     * 否则输出系统时间和秒杀时间
+     * @param seckillId
+     */
+    Exposer exportSeckillUrl(long seckillId);
+
+    /**
+     * 执行秒杀操作
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     */
+    void executeSeckill(long seckillId,long userPhone ,String md5);
+}
