7e9bdd8fa767febe12c45bc8b211aeed5903c83f
Coffee Flavor <1239142342@qq.com>
2016-09-27 17:59:45 +0800

    add core

---
+++ J2EE/servlet3/src/io/AsyncServlet.java
@@ -0,0 +1,13 @@
+package io;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * User: jennie
+ * Date: 2016/9/27
+ * Time: 17:33
+ */
+@WebServlet(urlPatterns = "/io/async")
+public class AsyncServlet extends HttpServlet {
+}
