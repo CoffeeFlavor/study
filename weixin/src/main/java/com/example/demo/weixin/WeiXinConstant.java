41555405e2aff4a00e05c7077e577723042f2f9e
Coffee Flavor <1239142342@qq.com>
2017-07-09 12:21:02 +0800

    delete

--- weixin/src/main/java/com/example/demo/weixin/WeiXinConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.weixin;
-
-/**
- * Created by jennie on 2017/7/7.
- */
-public class WeiXinConstant {
-
-    public static String WEIXIN_GET_TOKEN="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
-}
