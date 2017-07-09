ef1809fbe64923c1318f2fd7d93466e1a8e70455
Coffee Flavor <1239142342@qq.com>
2017-07-09 23:53:17 +0800

    add

--- weixin/src/main/java/com/example/demo/weixin/WeiXinConstant.java
+++ weixin/src/main/java/com/example/demo/weixin/WeiXinConstant.java
@@ -5,5 +5,7 @@ package com.example.demo.weixin;
  */
 public class WeiXinConstant {
 
-    public static String WEIXIN_GET_TOKEN="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+    public static String WEIXIN_GET_TOKEN="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s";
+
+    public static String WEIXIN_WEB_AUTHORIZE="https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=STATE#wechat_redirect";
 }
--- weixin/src/main/java/com/example/demo/weixin/WxBaseController.java
+++ weixin/src/main/java/com/example/demo/weixin/WxBaseController.java
@@ -1,18 +1,28 @@
 package com.example.demo.weixin;
 
 import com.example.demo.util.SignUtil;
+import com.sun.deploy.util.URLUtil;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import me.chanjar.weixin.mp.api.WxMpConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import javax.print.DocFlavor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by jennie on 2017/7/7.
@@ -20,6 +30,18 @@ import java.io.PrintWriter;
 
 @Controller
 public class WxBaseController  {
+
+    @Value("${weixin.appid}")
+    private String appId;
+
+    @Value("${weixin.appsecret}")
+    private String appSecret;
+
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("/check")
     @ResponseBody
@@ -60,6 +82,31 @@ public class WxBaseController  {
         return echostr;
     }
 
+    @RequestMapping("/get/weixin/token")
+    @ResponseBody
+    public Object getWeiXinToken(){
+        String url=String.format(WeiXinConstant.WEIXIN_GET_TOKEN,appId,appSecret);
+        RestTemplate restTemplate= new RestTemplate();
+        System.out.println(url);
+        ReWXToken wxToken= restTemplate.getForEntity(url,ReWXToken.class).getBody();
 
+        if (wxToken!=null&&wxToken.getAccess_token()!=null){
+//            stringRedisTemplate
+        }
+        return wxToken;
+    }
 
+    @RequestMapping("/test")
+    public String test(){
+        String redirectUrl=String.format(WeiXinConstant.WEIXIN_WEB_AUTHORIZE,appId, URLEncoder.encode("http://jennie.51vip.biz/redirectTest"),"snsapi_userinfo");
+        System.out.println(redirectUrl);
+        return redirectUrl;
+    }
+
+    @RequestMapping("/redirectTest")
+    public String redirectTest( String code, String state) {
+        System.out.println(code);
+        System.out.println(state);
+        return "test";
+    }
 }
