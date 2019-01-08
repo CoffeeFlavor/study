b91f1df294600226b2d24057d9073564d9966649
seven year <1239142342@qq.com>
2019-01-08 14:57:10 +0800

    异步返回线程设计

--- thread-core-gao/src/future/Client.java
+++ thread-core-gao/src/future/Client.java
@@ -6,5 +6,17 @@ package future;
  * time: 14:40
  */
 public class Client {
-
+    public Data request(String param) {
+        //立即返回FutureData
+        FutureData futureData = new FutureData();
+        //开启ClientThread线程装配RealData
+        new Thread(() -> {
+            {
+                //装配RealData
+                RealData realData = new RealData(param);
+                futureData.setRealData(realData);
+            }
+        }).start();
+        return futureData;
+    }
 }
