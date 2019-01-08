7ea1703046385fe69f5367845d9bba6d72552dd1
seven year <1239142342@qq.com>
2019-01-08 15:34:40 +0800

    111

--- thread-core-gao/src/future/Main.java
+++ thread-core-gao/src/future/Main.java
@@ -7,18 +7,18 @@ package future;
  */
 public class Main {
     public static void main(String[] args) {
-//        Client client = new Client();
-////        Data data = client.request("Hello Future!");
-////        System.out.println("请求完毕！");
-////
-////        try {
-////            //模拟处理其他业务
-////            Thread.sleep(2000);
-////        } catch (InterruptedException e) {
-////            e.printStackTrace();
-////        }
-////
-////        System.out.println("真实数据：" + data.getResult());
+        Client client = new Client();
+        Data data = client.request("Hello Future!");
+        System.out.println("请求完毕！");
+
+        try {
+            //模拟处理其他业务
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("真实数据：" + data.getResult());
 
         try {
             throw new RuntimeException("哈哈哈");
