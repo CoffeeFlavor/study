f65738fd657a9786f5de88f3adda5c527524b51e
seven <liunengbiao@syncpo.com>
2019-07-12 06:57:15 +0800

    maven study

--- maven-study/account/account-email/src/test/java/com/jennie/study/account/email/AccountEmailServiceTest.java
+++ maven-study/account/account-email/src/test/java/com/jennie/study/account/email/AccountEmailServiceTest.java
@@ -23,30 +23,32 @@ import static org.junit.Assert.*;
 public class AccountEmailServiceTest {
 
     private GreenMail greenMail;
+    private AccountEmailService accountEmailService;
 
     @Before
-    public void startMailServer()  throws Exception {
-        greenMail=new GreenMail(ServerSetup.SMTP);
-        greenMail.setUser("test3@jennie.com","123456");
+    public void startMailServer() throws Exception {
+        greenMail = new GreenMail(ServerSetup.SMTP);
+        greenMail.setUser("test3@jennie.com", "123456");
         greenMail.start();
     }
 
     @Test
-    public void  testSendEmail() throws Exception{
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("account-email.xml");
-        AccountEmailService accountEmailService=(AccountEmailService) ctx.getBean("accountEmailService");
-        String subject="Text Subject";
-        String htmlText="<h3>Test</h3>";
-        accountEmailService.sendEmail("1239142342@qq.com",subject,htmlText);
-        greenMail.waitForIncomingEmail(2000,1);
-        Message[] msgs=greenMail.getReceivedMessages();
-        assertEquals(1,msgs.length);
+    public void testSendEmail() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("account-email.xml");
+         accountEmailService = (AccountEmailService) ctx.getBean("accountEmailService");
+        String subject = "Text Subject";
+        String htmlText = "<h3>Test</h3>";
+        accountEmailService.sendEmail("1239142342@qq.com", subject, htmlText);
+        greenMail.waitForIncomingEmail(2000, 1);
+        Message[] msgs = greenMail.getReceivedMessages();
+        assertEquals(1, msgs.length);
         assertEquals(htmlText, GreenMailUtil.getBody(msgs[0]).trim());
     }
 
     @After
-    public void stopMailServer() throws Exception{
-        greenMail.stop();;
+    public void stopMailServer() throws Exception {
+        greenMail.stop();
+        ;
     }
 
 }
