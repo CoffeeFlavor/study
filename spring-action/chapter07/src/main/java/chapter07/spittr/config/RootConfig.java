19fa9dbc5569e0ddc9e06f1ac14e74305a377e63
seven <seven@github.com>
2019-08-17 18:16:22 +0800

    spring action

---
+++ spring-action/chapter07/src/main/java/chapter07/spittr/config/RootConfig.java
@@ -0,0 +1,19 @@
+package chapter07.spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : jennie
+ * @date: 2019/8/4
+ * @Time: 15:00
+ */
+@Configuration
+@ComponentScan(basePackages = {"chapter07.spittr"}
+,excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION,value = EnableWebMvc.class)
+})
+public class RootConfig {
+}
